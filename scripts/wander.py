c6ff00601e6a5e0c5867a0538e670a2fd0f1b658
Gustavo Velasco Hernández <gustavo.velascoh@gmail.com>
2013-07-02 12:13:16 -0500

    Included service for start wandering

--- scripts/wander.py
+++ scripts/wander.py
@@ -3,11 +3,14 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from vintersim.srv import *
 
 robot_pose = Odometry()
 next_goal = Odometry()
 
 robot_vel = Twist()
+wander = 0
+turn = 0;
 
 # Functions
 def set_goal():
@@ -18,11 +21,22 @@ def move_to():
 
 # Services
 
-def start_wander():
-	pass
+def start_wander(req):
+	global wander
+	wander = 1
+	rospy.loginfo("starting wander...")
+	return wander
 
-def stop_wander():
-	pass
+def stop_wander(req):
+	global wander
+	wander = 0
+	rospy.loginfo("stoping wander...")
+	return wander
+
+def turn():
+	global turn
+	turn = 1
+	rospy.loginfo("turning...")
 
 # Subscriber callback
 def pose_subscriber(data):
@@ -43,11 +57,21 @@ def talker():
 
 if __name__ == '__main__':
 	try:
-		vel_pub = rospy.Publisher('/robot_2/cmd_vel',Twist);
+		vel_pub = rospy.Publisher('/car_0/cmd_vel',Twist)
 		rospy.init_node('wander')
-		rospy.Subscriber("/robot_2/odom", Odometry, pose_subscriber)
+		rospy.Subscriber("/car_0/odom", Odometry, pose_subscriber)
+		rospy.Service('/car_0/start_wander', WanderCtrl, start_wander)
+		rospy.Service('/car_0/stop_wander', WanderCtrl, stop_wander)
+		rospy.Service('/car_0/turn', WanderCtrl, turn)
+		
 		while not rospy.is_shutdown():
 			rospy.loginfo("my Pose is (%s,%s)" % (robot_pose.pose.pose.position.x,robot_pose.pose.pose.position.y))
-			rospy.sleep(1.0)
+			if wander:
+				x_0 = robot_pose.pose.pose.position.x
+				robot_vel.linear.x = 5
+				vel_pub.publish(robot_vel)
+			else:	
+				rospy.sleep(1.0)
+			
 	except rospy.ROSInterruptException:
 		pass
