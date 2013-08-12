8250cbb6ad97ad609899677d3ba1b0980df0735d
Gustavo Velasco Hernández <gustavo.velascoh@gmail.com>
2013-08-12 14:34:12 -0500

    Changed topic names in parameter for costmaps. Setting default value for laser topic names.

--- vinter_control/scripts/simple_intersection_reduced.py
+++ vinter_control/scripts/simple_intersection_reduced.py
@@ -63,7 +63,8 @@ if __name__ == '__main__':
 	my_int = intersection = Intersection()
 	my_int.set_type('crossroad')
 	
-	laser_list_str = rospy.get_param('laser_base_list', '')
+    
+	laser_list_str = rospy.get_param('laser_base_list', 'base_0 base_1 base_2 base_3')
 	laser_ns = laser_list_str.split(' ')
 	rospy.loginfo('laser_list = %s' % laser_list_str)
 	
